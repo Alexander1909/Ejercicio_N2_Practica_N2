89ff2f44cf1efee4c901ff36de30be54c5ff624c
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-03-27 10:53:53 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/grilladeimagenes/MainActivity.kt
+++ app/src/main/java/com/example/grilladeimagenes/MainActivity.kt
@@ -6,15 +6,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gridView: GridView
+
+    private val pizzas = arrayOf(
+        "Margarita",
+        "Pepperoni",
+        "Hawaiana",
+        "Cuatro Quesos",
+        "Vegetariana",
+        "BBQ"
+    )
+
+    private val imagenes = intArrayOf(
+        R.drawable.margarita,
+        R.drawable.pepperoni,
+        R.drawable.hawaiana,
+        R.drawable.cuatro_quesos,
+        R.drawable.vegetariana,
+        R.drawable.bbq
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        gridView = findViewById(R.id.gridView)
+
+        val adapter = PizzaAdapter(this, pizzas, imagenes)
+        gridView.adapter = adapter
+
+        gridView.setOnItemClickListener { _, _, position, _ ->
+            Toast.makeText(this, "Pizza: ${pizzas[position]}", Toast.LENGTH_SHORT).show()
         }
     }
 }
